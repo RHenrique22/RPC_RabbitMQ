6862198bcd4c7c555c7f7ff132cf793c5306f15f
RHenrique <roosevelthenrique22@gmail.com>
2022-10-08 22:56:08 -0300

    feat: RPC RabbitMQ

---
+++ src/main/java/br/edu/ifpb/prog_dist/RPCServer.java
@@ -0,0 +1,64 @@
+package br.edu.ifpb.prog_dist;
+
+import java.util.StringTokenizer;
+
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+
+public class RPCServer {
+
+    private static final String RPC_QUEUE_NAME = "rpc_queue";
+
+    private static int fib(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        return fib(n - 1) + fib(n - 2);
+    }
+
+    public static void main(String[] argv) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setUsername("mqadmin");
+        factory.setPassword("Admin123XX_");
+        factory.setHost("localhost");
+        factory.setPort(5672);
+        
+
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+        
+        channel.queueDeclare(RPC_QUEUE_NAME, false, false, false, null);
+        channel.queuePurge(RPC_QUEUE_NAME);
+
+        channel.basicQos(1);
+
+        System.out.println(" [x] Awaiting RPC requests");
+
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            AMQP.BasicProperties replyProps = new AMQP.BasicProperties
+                    .Builder()
+                    .correlationId(delivery.getProperties().getCorrelationId())
+                    .build();
+
+            String response = "";
+            try {
+                String message = new String(delivery.getBody(), "UTF-8");
+                StringTokenizer split = new StringTokenizer(message, ":");
+                split.nextToken();
+                int n = Integer.parseInt(split.nextToken());
+
+                System.out.println(" [.] fib(" + message + ")");
+                response += "Roosevelt Henrique de Lima Ferreira respondeu:" + fib(n);
+            } catch (RuntimeException e) {
+                System.out.println(" [.] " + e);
+            } finally {
+                channel.basicPublish("", delivery.getProperties().getReplyTo(), replyProps, response.getBytes("UTF-8"));
+                channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
+            }
+        };
+
+        channel.basicConsume(RPC_QUEUE_NAME, false, deliverCallback, (consumerTag -> {}));
+    }
+}
